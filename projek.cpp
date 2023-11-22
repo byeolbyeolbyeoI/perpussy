9f46437b363fb3ead95a728e306129b4db9a4e98
goatyoda <mash1o1o1o1@gmail.com>
2023-11-22 12:00:14 +0700

    perlu revisi pinjam, kembali, flow, setwfill

--- projek.cpp
+++ projek.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <fstream>
+#include <stdio.h>
 
 using namespace std;
 
@@ -28,6 +29,7 @@ struct Buku {
 
 struct Mahasiswa {
     string nim;
+    string nama;
     string prodi;
     string status;
 };
@@ -295,8 +297,192 @@ void edit(){
 }
 
 void pinjam(){
+    struct Buku Pinjam;
+    struct Buku P;
+    struct Menu P1;
+    struct Mahasiswa M;
+    struct Mahasiswa Mahasiswa;
 
+    P1.count = 0;
+    P1.count2 = 0;
+    ifstream pinjam("buku.txt");
+
+    pinjam.seekg(0, ios::end);
+
+    if(pinjam.tellg() < 1){
+        cout << "kosong";
+    }
+    else{
+        pinjam.clear();
+        pinjam.seekg(0,ios::beg);
+        
+        do{
+            P.id.clear();
+            cin.ignore();
+            cout << "masukkan id yang ingin dipinjam : ";
+            cin >> P.id;
+            cout << P.id;
+
+            while(
+                getline(pinjam, Pinjam.id)         &&
+                getline(pinjam, Pinjam.judul)      &&
+                getline(pinjam, Pinjam.genre)      &&
+                getline(pinjam, Pinjam.penulis)    &&
+                getline(pinjam, Pinjam.penerbit)   &&
+                getline(pinjam, Pinjam.tahun)      &&
+                getline(pinjam, Pinjam.status)   
+            )
+            {
+                if(P.id == Pinjam.id){
+                    if(Pinjam.status == "Siap"){
+                        cout << "Buku tersedia!\n";
+                        do{
+                            cout << "masukkan nim peminjam : ";
+                            cin >> M.nim;
+
+                            ifstream p("mahasiswa.txt");
+                            while(
+                                getline(p, Mahasiswa.nim)       &&
+                                getline(p, Mahasiswa.nama)      &&
+                                getline(p, Mahasiswa.prodi)     &&
+                                getline(p, Mahasiswa.status)
+                            )
+                            {
+                                if(M.nim == Mahasiswa.nim){
+                                    string oleh = "Dipinjam oleh ";
+                                    Pinjam.status = oleh.append(Mahasiswa.nama);
+                                    cout << Pinjam.status;
+                                    P1.count = 1;
+                                    break;
+                                }
+                            }
+                            if(P1.count == 1){
+                                break;
+                            }
+                        }while(true);
+                    }
+                    else{
+                        cout << "Buku sudah dipinjam, pilih buku lain\n";
+                        P1.count2 = 1;
+                        break;
+                    }
+                    if(P1.count2 == 1){
+                        break;
+                    }
+                }
+                cout << "SAMPE SINI";
+                if(P1.count2 == 1){
+                    break;
+                }
+                ofstream pinjam2("buku3.txt", ios::app);
+                pinjam2 << Pinjam.id << endl << Pinjam.judul << endl << Pinjam.genre << endl << Pinjam.penulis << endl << Pinjam.penerbit << endl << Pinjam.tahun << endl << Pinjam.status << endl;
+                
+            }
+            if(P1.count == 1){
+                break;
+            }
+        }while(true);
+        pinjam.close();
+
+        remove("buku.txt");
+        rename("buku3.txt", "buku.txt");
+    }
 }
+void kembali(){
+    struct Buku Kembali;
+    struct Buku K;
+    struct Menu K1;
+    struct Mahasiswa M;
+    struct Mahasiswa Mahasiswa;
+
+    K1.count = 0;
+    K1.count2 = 0;
+    ifstream kembali("buku.txt");
+
+    kembali.seekg(0, ios::end);
+
+    if(kembali.tellg() < 1){
+        cout << "kosong";
+    }
+    else{
+        kembali.clear();
+        kembali.seekg(0,ios::beg);
+        
+        do{
+            K.id.clear();
+            cin.ignore();
+            cout << "masukkan id yang ingin dipinjam : ";
+            cin >> K.id;
+            cout << K.id;
+
+            while(
+                getline(kembali, Kembali.id)         &&
+                getline(kembali, Kembali.judul)      &&
+                getline(kembali, Kembali.genre)      &&
+                getline(kembali, Kembali.penulis)    &&
+                getline(kembali, Kembali.penerbit)   &&
+                getline(kembali, Kembali.tahun)      &&
+                getline(kembali, Kembali.status)   
+            )
+            {
+                cout << "ga masuk sini";
+                if(K.id == Kembali.id){
+                    if(Kembali.status == "Siap"){
+                        cout << "Buku tersedia!\n";
+                        do{
+                            cout << "masukkan nim peminjam : ";
+                            cin >> M.nim;
+
+                            ifstream k("mahasiswa.txt");
+                            while(
+                                getline(k, Mahasiswa.nim)       &&
+                                getline(k, Mahasiswa.nama)      &&
+                                getline(k, Mahasiswa.prodi)     &&
+                                getline(k, Mahasiswa.status)
+                            )
+                            {
+                                if(M.nim == Mahasiswa.nim){
+                                    string oleh = "Dipinjam oleh ";
+                                    Kembali.status = oleh.append(Mahasiswa.nama);
+                                    cout << Kembali.status;
+                                    K1.count = 1;
+                                    break;
+                                }
+                            }
+                            if(K1.count == 1){
+                                break;
+                            }
+                        }while(true);
+                        cout << "masuk sini kah";
+                    }
+                    else{
+                        cout << "Buku sudah dipinjam, pilih buku lain\n";
+                        K1.count2 = 1;
+                        break;
+                    }
+                    cout << "masuk sini?";
+                    if(K1.count2 == 1){
+                        break;
+                    }
+                }
+                if(K1.count2 == 1){
+                    break;
+                }
+                ofstream kembali2("buku3.txt", ios::app);
+                kembali2 << Kembali.id << endl << Kembali.judul << endl << Kembali.genre << endl << Kembali.penulis << endl << Kembali.penerbit << endl << Kembali.tahun << endl << Kembali.status << endl;
+                
+            }
+            if(K1.count == 1){
+                break;
+            }
+        }while(true);
+        kembali.close();
+
+        remove("buku.txt");
+        rename("buku3.txt", "buku.txt");
+    }
+}
+
 int main(){
     struct Pengelola A; 
     
@@ -317,6 +503,7 @@ int main(){
     }while(true);
 
     int menu; //debug
+    int menu4;
     do{
         system("cls");
         cout << "Menu\n[1]. Lihat Daftar Buku\n[2]. Input Buku\n[3]. Edit Buku\n[4]. Peminjaman\nPilih : ";
@@ -332,7 +519,22 @@ int main(){
                 edit();
                 break;
             case 4 :
-                pinjam();
+                do{
+                    cout << "Menu :\n[1]. Pinjam\n[2]. Kembalikan\n";
+                    cout << "Pilih : ";
+                    cin >> menu4;
+                    switch(menu4){
+                        case 1:
+                            pinjam();
+                            break;
+                        case 2:
+                            kembali();
+                            break;
+                        default:
+                            cout << "input tidak valid, silahkan mengisi kembali";
+                            break;    
+                    }
+                }while(true);
                 break;
             default :
                 cout << "input tidak valid, silahkan memilih kembali";
